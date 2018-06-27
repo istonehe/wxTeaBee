76871f06e34fb2f04f7d7c0268e5a29bc6bf8994
stonehe <didahuang@gmail.com>
2018-06-28 01:09:07 +0800

    register ui

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,29 +1,51 @@
-//index.js
-//获取应用实例
+//login.js
 const app = getApp()
 const auth = require('../../utils/auth.js')
-
+const requests = require('../../utils/requests.js')
 Page({
-  data: {
-    motto: 'Hello World',
+  data:{
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
+  onLoad:function(){
+    let that = this;
+    // 检查是否有token和用户信息
+    let token = wx.getStorageSync('token') || '';
+    let teacher_info = wx.getStorageSync('teacher_info') || {};
+
+    if (token && (Object.keys(teacher_info).length != 0) ) {
+      //使用token获取用户信息
+      requests.getTeacherInfoPromise().then(
+        () => {
+          if (teacher_info.register) {
+            wx.redirectTo({
+              url: '../asklist/asklist'
+            })
+          }
+        }
+      );
+
+    } else {
+      //登录换取token
+      auth.beeLoginPromise().then(
+        function () {
+          let teacher_info = wx.getStorageSync('teacher_info') || {};
+          if (teacher_info.register){
+            wx.redirectTo({
+              url: '../asklist/asklist'
+            })
+          }
+        }
+      );
+    };
 
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -44,8 +66,9 @@ Page({
         }
       })
     }
+
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
--- utils/requests.js
+++ utils/requests.js
@@ -3,7 +3,39 @@
 const config = require('config.js');
 const url = config.config.host;
 const base64 = require('base64.min.js').Base64;
+const auth = require('auth.js')
 
-//wx.request({
-//  url: url + '/v1/teacher/',
-//})
+function getTeacherInfoPromise(){
+  let teacher_info = wx.getStorageSync('teacher_info')
+  let teacher_id = teacher_info.id
+  let token = wx.getStorageSync('token')
+  let getTeacherInfo = new Promise((resolve, reject) => {
+    wx.request({
+      url: url + '/v1/school/teacher/' + teacher_id,
+      header: {'Authorization': 'Basic ' + base64.encode(token + ':x')},
+      success: (res) => {
+        console.log(res.data)
+        let data = res.data
+        if (data.code == 4) {
+          auth.beeLoginPromise();
+        } else if (data.code == 1) {
+          resolve(data);
+          let user_info = data.teacher;
+          wx.setStorageSync('teacher_info', user_info);
+        } else {
+          wx.showToast({
+            title: data.message || '服务器开小差了',
+            icon: 'none',
+            duration: 2000
+          });
+          reject();
+        }
+      }
+    })
+  })
+  return getTeacherInfo;
+}
+
+module.exports = {
+  getTeacherInfoPromise: getTeacherInfoPromise
+}
